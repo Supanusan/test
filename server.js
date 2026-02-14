6a0c1ee3feff9b612c4a2d60435d2db0e7bc8db4
Supanusan <ssupanusan@gmail.com>
2026-02-14 20:51:08 +0530

    cors

--- server.js
+++ server.js
@@ -16,10 +16,14 @@ configDotenv();
 //middlewares
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
     credentials: true,
-  }),
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
 );
+app.options("*", cors());
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
